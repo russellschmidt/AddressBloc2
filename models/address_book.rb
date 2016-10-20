0705e2e8939c6e67d0c3e7aa76950e7932fd3ecd
Russell Schmidt <russell.schmidt@me.com>
2016-10-19 22:37:12 -0700

    add find_entry_by_id to models/address_book.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,6 +18,10 @@ class AddressBook < BlocRecord::Base
     Entry.where(name: name, address_book_id: self.id).first
   end
 
+  def find_entry_by_id(id)
+    Entry.where(id: id, address_book_id: self.id).first
+  end
+
   def import_from_csv(file_name)
     # Implementation goes here
     csv_text = File.read(file_name)
